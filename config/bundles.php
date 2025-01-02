00be98557cd7c916d877caa9f0481fddc4512e8d
MattDaemon1 <matt@mattkonnect.com>
2025-01-02 18:18:31 +0100

    install vichuploader

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
