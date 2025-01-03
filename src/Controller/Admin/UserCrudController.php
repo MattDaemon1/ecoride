d93cf18497b968a67c222f42212be1f460608e74
MattDaemon1 <matt@mattkonnect.com>
2025-01-03 01:37:57 +0100

    amélioration front

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,9 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use Vich\UploaderBundle\Form\Type\VichImageType; 
 
 class UserCrudController extends AbstractCrudController
 {
@@ -15,14 +19,23 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')->hideOnForm();
+        yield EmailField::new('email'); 
+        yield TextField::new('password'); 
+        yield TextField::new('nom'); 
+        yield TextField::new('prenom'); 
+        yield TextField::new('adresse'); 
+        yield TextField::new('telephone'); 
+        yield DateField::new('dateNaissance'); 
+        yield TextField::new('pseudo'); 
+        yield TextareaField::new('imageFile')
+            ->setFormType(VichImageType::class)->hideOnIndex()
+            ->setLabel('Image')
+            ->setRequired(false);
+        yield ImageField::new('image')
+            ->setBasePath('/images/users')
+            ->hideOnForm();
     }
-    */
 }
