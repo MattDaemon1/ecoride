80176c3b90f1dbfa46622d1403059e7be0788b06
MattDaemon1 <matt@mattkonnect.com>
2025-01-03 02:23:22 +0100

    coix multiple énergie entité voiture

--- src/Controller/Admin/VoitureCrudController.php
+++ src/Controller/Admin/VoitureCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Voiture;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -23,7 +24,13 @@ class VoitureCrudController extends AbstractCrudController
         yield TextField::new('modele');
         yield TextField::new('immatriculation');
         yield TextField::new('couleur');
-        yield TextField::new('energie');
+        yield ChoiceField::new('energie')
+        ->setChoices([
+            'Électrique' => 'Électrique',
+            'Essence' => 'Essence',
+            'Diesel' => 'Diesel',
+            'Autre' => 'Autre',
+        ]);
         yield DateTimeField::new('datePremiereImmatriculation');
         yield AssociationField::new('marque');
         yield AssociationField::new('user'); 
--- src/Entity/Voiture.php
+++ src/Entity/Voiture.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: VoitureRepository::class)]
 class Voiture
@@ -23,6 +24,7 @@ class Voiture
     private ?string $immatriculation = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\Choice(choices: ['Électrique', 'Essence', 'Diesel', 'Autre'], message: 'Choisissez une énergie valide.')]
     private ?string $energie = null;
 
     #[ORM\Column(length: 50)]
